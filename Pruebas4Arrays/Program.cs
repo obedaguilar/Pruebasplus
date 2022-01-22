3127daf8f883cdd85f0d1ac3c2382cbe36af1a27
root <root@DESKTOP-G284E7C.localdomain>
2022-01-22 17:43:19 -0600

    acá nada más usamos foreach

--- Pruebas4Arrays/Program.cs
+++ Pruebas4Arrays/Program.cs
@@ -6,10 +6,20 @@ Console.WriteLine("Hello, World!");
 //y cuantos son impares
 
 int[] valores = { 7, 9, 23, 56, 23, 34, 66, 78, 79, 34, 12, 16, 15 };
+//int nelementos = valores.Length;
+//forma trabajosa como dice mi cuate 
+//for (int i = 0; i < nelementos; i++)
+//{
+//    Console.WriteLine(valores[i]);
 
-//foreach (int val in valores)
+//}
+
+foreach (int val in valores) { 
+    Console.WriteLine(val);
+}
     
 
-Console.WriteLine("valores" + valores.Length);
-Console.WriteLine("valores" + valores[0]);
-Console.WriteLine("valores" + valores[2]);
+//Console.WriteLine("valores" + valores.Length);
+//Console.WriteLine("valores" + valores[0]);
+//Console.WriteLine("el ultimo elemento" + valores[valores.Length -1]);
+
