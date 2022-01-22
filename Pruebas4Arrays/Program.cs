4e8aa72592e299b96dee596e8c428560cc036514
root <root@DESKTOP-G284E7C.localdomain>
2022-01-22 17:30:46 -0600

    aca le agregamos arrays a la cosa

---
+++ Pruebas4Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Se tiene el siguiente array de números . Calcular cuantos elementos son pares
+//y cuantos son impares
+
+int[] valores = { 7, 9, 23, 56, 23, 34, 66, 78, 79, 34, 12, 16, 15 };
+
+//foreach (int val in valores)
+    
+
+Console.WriteLine("valores" + valores.Length);
+Console.WriteLine("valores" + valores[0]);
+Console.WriteLine("valores" + valores[2]);
